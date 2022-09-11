f8a398b6c67c90bf1fa7372aae6e8eaf28a4e427
Adin W-T <adinrwt@gmail.com>
2022-09-10 22:44:41 -0700

    Add one off formatting to drill down formatter

--- drill_down_formatter.py
+++ drill_down_formatter.py
@@ -1,6 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+
+def process_one(dot_oid):
+    result = "DRILL_DOWN_OID = ["
+
+    split_oid = dot_oid.split(".")
+
+    for i in range(1, len(split_oid) + 1):
+        result += f'"{".".join(split_oid[0:i])}"'
+
+        if i != len(split_oid):
+            result += ", "
+
+    result += "]"
+
+    return result
+
 
 def main():
     in_str = " "
+
+    if len(sys.argv) > 1:
+        print(process_one(sys.argv[1]))
+        return
 
     while True:
         in_str = input("start_oid> ").strip()
@@ -8,16 +31,7 @@ def main():
         if in_str == "" or in_str.lower() == "exit":
             return
 
-        split_str = in_str.split(".")
-
-        print("DRILL_DOWN_OID = [", end="")
-        for i in range(1, len(split_str) + 1):
-            print(f'"{".".join(split_str[0:i])}"', end="")
-
-            if i != len(split_str):
-                print(", ", end="")
-
-        print("]\n\n", end="")
+        print(process_one(in_str), end="\n\n")
 
 
 if __name__ == "__main__":
