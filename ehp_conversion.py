46fde7884b1e6b56beae76c2689cc5a8b50cabc1
Adin W-T <adinrwt@gmail.com>
2022-09-18 16:20:53 -0400

    Convert scrape_description_list to use ehp library

---
+++ ehp_conversion.py
@@ -0,0 +1,110 @@
+from typing import Dict, List
+
+import requests
+import ehp
+import dateparser
+
+ENTERPRISE_FILE_PATH = "convert_enterprise/1.3.6.1.4.1.html"
+
+def extract_list(tag: ehp.Root) -> List[str]:
+    result = []
+
+    inner_tags = [t for t in tag if type(t) == ehp.Tag]
+
+    for inner_tag in inner_tags:
+        result.append(inner_tag.text().strip())
+
+    
+    return result
+
+
+def scrape_description_list(root: ehp.Root) -> Dict:
+    data = {}
+
+    description_list = None
+    try:
+        description_list = list(root.find("dl"))[0]
+    except IndexError as e:
+        # Root doesn't contain a dl tag: Gulp
+        pass
+
+    if description_list == None:
+        # Skip scraping if Root doesn't contain a dl tag
+        return None
+
+    section_titles = [dt.text().strip().lower().replace(" ", "_") for dt in description_list.find("dt")]   
+    section_contents = list(description_list.find("dd"))
+
+    # for i in range(len(section_titles)):
+    #     print(section_titles[i])
+    #     print("-----------------")
+
+    #     if section_titles[i] in ["node_names", "asn1_oid", "iri_oid"]:
+    #         print(extract_list(section_contents[i]))
+    #     else:
+    #         print(list(section_contents[i]))
+
+    #     print()
+
+    # return {}
+
+    for i in range(len(section_titles)):
+        section_title = section_titles[i]
+        section_content = section_contents[i]
+        
+        if section_title == "node_name":
+            # Standardize differences in webpage dt naming
+            section_title = "node_names"
+
+        converted_content = None
+        match section_title:
+            case "parent":
+                anchor = section_content.fst("a")                
+
+                if not anchor:
+                    continue
+
+                converted_content = anchor.text().strip()
+                if converted_content == "None":
+                    # Special case for root nodes
+                    converted_content = None
+            case "node_code":
+                try:
+                    converted_content = int(section_content.text())
+                except:
+                    if section_content.text().strip() == "None":
+                        converted_content = None
+                    else:
+                        converted_content = section_content.text().strip()
+            case "node_names" | "asn1_oid" | "iri_oid":
+                converted_content = extract_list(section_content)
+            case "creation_date" | "modification_date":
+                # {"TIMEZONE": "UTC"} avoids a PytzUsageWarning
+                converted_content = dateparser.parse(section_content.text(), settings={"TIMEZONE": "UTC"})
+            case _:
+                converted_content = section_content.text().strip()
+
+        data[section_title] = converted_content
+
+    return data
+
+
+def main():
+    enterprise_file_contents = None
+    with open(ENTERPRISE_FILE_PATH, "rt") as enterprise_file:
+        enterprise_file_contents = enterprise_file.read()
+
+    response = requests.get("https://google.com")    
+
+    ehp_parser = ehp.Html()
+
+    root = ehp_parser.feed(enterprise_file_contents)
+    # root = ehp_parser.feed(response.text)
+
+    scraped_description_list = scrape_description_list(root)
+
+    print(scraped_description_list)
+
+
+if __name__ == "__main__":
+    main()
